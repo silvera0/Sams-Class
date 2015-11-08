af6f10e51dee5fa8b0f0928748330c9bdcb806f2
Ryan O'Donnell <rodonnell2008@gmail.com>
2015-11-08 14:06:31 -0600

    Week 1 Assignment

---
+++ RyanODonnell - Week 1/Assets/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour {
+	
+	public string broadcastText;
+
+	// Use this for initialization
+	void Start () {
+		print(broadcastText);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
