3237d234ded9d1bdf03490e38f9ae27d9b8d2bb9
Ryan O'Donnell <rodonnell2008@gmail.com>
2015-11-14 15:47:30 -0600

    Week 2 Part 1 Chaser

---
+++ RyanODonnell - Week 2/Assets/Chase.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chase : MonoBehaviour {
+
+	public GameObject targetObject;
+	public float moveSpeed;
+
+	private Transform chaser;
+	private Transform target;
+
+
+	// Use this for initialization
+	void Start () {
+	 	
+		chaser = transform;
+		target =  targetObject.transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 direction = (target.position - chaser.position).normalized;
+		Vector3 movement = direction * moveSpeed * Time.deltaTime;
+		Vector3 newPosition = chaser.position + movement;
+
+		transform.position = newPosition;
+
+	}
+}
